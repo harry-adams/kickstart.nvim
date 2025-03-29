6bed9524d2e82941661e388e1648045bf7553607
harry-adams <91059134+harry-adams@users.noreply.github.com>
2025-03-29 14:46:36 +0000

    feat(explorer): integrate oil.nvim file explorer with float window config
    - Add keymap for <leader>e to open Oil in a centered float
    - Enable support for hidden files and previews
    - Fix highlight groups for floating windows and borders

--- init.lua
+++ init.lua
@@ -235,22 +235,44 @@ require('lazy').setup({
   'github/copilot.vim',
 
   {
+    'ellisonleao/gruvbox.nvim',
+    priority = 1000,
+    config = function()
+      require('gruvbox').setup {
+        terminal_colors = true,
+        transparent_mode = true,
+      }
+
+      vim.cmd 'colorscheme gruvbox'
+
+      -- 👇 Set these *after* the colorscheme!
+      vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+      vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+      vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
+      vim.api.nvim_set_hl(0, 'OilNormalFloat', { bg = 'none' })
+      vim.api.nvim_set_hl(0, 'OilFloatBorder', { bg = 'none' })
+    end,
+  },
+
+  {
     'stevearc/oil.nvim',
     opts = {
-      -- See :help oil-config for a complete list of options
       view_options = {
-        -- Show files and directories that start with "."
         show_hidden = true,
         show_preview = true,
       },
       skip_confirm_for_simple_edits = true,
-      -- Customize the visible columns for a cleaner look
-      columns = {
-        'icon', -- file/folder icon
-        'size', -- file size
-        'mtime', -- last modified time
-      },
+      columns = { 'icon', 'size', 'mtime' },
       use_default_keymaps = true,
+      float = {
+        padding = 2,
+        max_width = 160,
+        max_height = 25,
+        border = 'rounded',
+        win_options = {
+          winblend = 0,
+        },
+      },
     },
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
@@ -262,16 +284,38 @@ require('lazy').setup({
         skip_confirm_for_simple_edits = true,
         columns = { 'icon', 'size', 'mtime' },
         use_default_keymaps = true,
+        float = {
+          padding = 10,
+          max_width = math.huge, -- Take up nearly full screen width
+          max_height = math.huge, -- Take up nearly full screen height
+          border = 'rounded',
+          win_options = {
+            winhighlight = 'Normal:NormalFloat,FloatBorder:FloatBorder',
+          },
+        },
       }
-
-      -- Optional: override highlights to better match your colorscheme
+      vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+      vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+      vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
+      vim.api.nvim_set_hl(0, 'OilFile', { link = 'Normal' })
+      vim.api.nvim_set_hl(0, 'OilDir', { link = 'Directory' })
+      vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+      vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
+      -- Highlight tweaks (optional)
       vim.api.nvim_set_hl(0, 'OilDir', { link = 'Directory' })
       vim.api.nvim_set_hl(0, 'OilFile', { link = 'Normal' })
 
-      -- Optional: open Oil in a floating window instead of the main view
-      vim.keymap.set('n', '<leader>e', function()
-        require('oil').open_float()
-      end, { desc = 'Open [E]xplorer (Oil Float)' })
+      -- Open Oil in floating window (centered by above config)
+      vim.keymap.set('n', '<leader>e', require('oil').open_float, { desc = 'Open [E]xplorer (Oil Float)' })
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = 'oil',
+        callback = function(args)
+          vim.keymap.set('n', '<Esc>', '<cmd>close<CR>', {
+            buffer = args.buf,
+            desc = 'Close Oil with Esc',
+          })
+        end,
+      })
     end,
   },
 
@@ -879,24 +923,6 @@ require('lazy').setup({
           { name = 'path' },
         },
       }
-    end,
-  },
-
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'retrobox'
-
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
     end,
   },
 
